89c08c9a8f94e5802cae15aa3136694354368f6e
Aadhil Nandan <aadhianddhilusworld@gmail.com>
2025-09-30 18:46:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,23 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/tiles/<int:z>/<int:y>/<int:x>.png") 
-def get_tile(z, y, x): 
-    
+@app.route("/tiles/<string:z>/<int:y>/<int:x>.png")
+def get_tile(z, y, x):
+
+    try:
+        z_int = int(z)
+    except ValueError:
+        abort(404) 
+
     if x < 0 or y < 0:
-        abort(404)    
-    filepath = f"tiles/moon/{z}/{y}/{x}.png"
-    
+        abort(404)
+
+    filepath = f"tiles/{z}/{y}/{x}.png"
+
     if os.path.exists(filepath):
         return send_file(filepath, mimetype="image/png")
     else:
         abort(404)
 
 if __name__ == '__main__':
-    print(f"Starting server. Current working directory: {os.getcwd()}")
     app.run(debug=True, port=5000)
-
